00a5e5858c8c959623af53b456e554a19aa89d2d
Hy Huynh <hyxhuynh.projects@gmail.com>
2024-04-29 06:17:08 -0700

    initial commit

--- app.js
+++ app.js
@@ -3,8 +3,27 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
-
-// Exercise 2
+// Exercise 1 and 2
+const firstName = "Michelle";
+let lastName = "Huynh";
+var age = 21;
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+// Excercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
